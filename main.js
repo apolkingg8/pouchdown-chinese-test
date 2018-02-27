16e6cc5478d19d24c70ffae6ef6786d7587e27c1
EijiHsu <apolkingg8@gmail.com>
2018-02-27 14:52:28 +0800

    update

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ leveldown(path.resolve(__dirname, '中文', '123')).open(function (res) {
     debugger
 })
 
-const PouchDB = require('pouchdb-core')
+/*const PouchDB = require('pouchdb-core')
 PouchDB.plugin(require('pouchdb-adapter-rocksdb').default)
 
-let test = new PouchDB(path.resolve(__dirname, '中文2', '123'), {adapter: 'rocksdb'})
+let test = new PouchDB(path.resolve(__dirname, '中文2', '123'), {adapter: 'rocksdb'})*/
