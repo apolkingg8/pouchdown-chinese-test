e23ea77d153efc1f1024de1df70fbf35e61497b9
apolkingg8 <apolkingg8@gmail.com>
2018-02-27 14:46:20 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const leveldown = require("rocksdb")
+const path = require("path")
+
+leveldown(path.resolve(__dirname, '中文', '123')).open(function (res) {
+    debugger
+})
+
+const PouchDB = require('pouchdb-core')
+PouchDB.plugin(require('pouchdb-adapter-rocksdb').default)
+
+let test = new PouchDB(path.resolve(__dirname, '中文2', '123'), {adapter: 'rocksdb'})
